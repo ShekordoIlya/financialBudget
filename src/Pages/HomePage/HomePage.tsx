571d857092ad726ebba16e8acd3814f9bad679a9
Шекордо Илья Сергеевич <shekordo_is@peleng.jsc.local>
2026-01-06 13:52:50 +0300

    feat(styles): backgrounds was added for some sections of the project

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,5 +1,11 @@
+import styles from "./HomePage.module.scss";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.homePageContainer}>
+      <p>HOME PAGE</p>
+    </div>
+  );
 };
 
 export default HomePage;
