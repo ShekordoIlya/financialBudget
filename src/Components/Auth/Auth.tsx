f0409db94fb723e100c141e13891b361b87cb232
Шекордо Илья Сергеевич <shekordo_is@peleng.jsc.local>
2026-01-19 16:40:21 +0300

    feat(app): GSAP was added to the project

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,10 +1,19 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../../Store/store";
 import style from "./Auth.module.scss";
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import Button from "../Button/Button";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+// gsap.registerPlugin(useGSAP);
 
 const Auth = () => {
+  //   const container = useRef<HTMLDivElement>(null);
+  //   const btnWrapperRef = useRef<HTMLDivElement>(null);
+  //   const xTo = useRef<gsap.QuickToFunc | null>(null);
+  //   const yTo = useRef<gsap.QuickToFunc | null>(null);
+
   const { name, userName, password } = useSelector((state: RootState) => state.Ilya);
   const [username, setUsername] = useState("");
   const [userPassword, setUserPassword] = useState("");
@@ -13,17 +22,58 @@ const Auth = () => {
 
   const handlerLogin = () => {};
 
+  //   const { contextSafe } = useGSAP(() => {
+  //     if (btnWrapperRef.current) {
+  //       xTo.current = gsap.quickTo(btnWrapperRef.current, "x", {
+  //         duration: 0.8,
+  //         ease: "power3",
+  //       });
+  //       yTo.current = gsap.quickTo(btnWrapperRef.current, "y", {
+  //         duration: 0.8,
+  //         ease: "power3",
+  //       });
+  //     }
+  //   });
+
+  //   const moveShape = useCallback(
+  //     contextSafe((e: React.MouseEvent) => {
+  //       if (xTo.current && yTo.current) {
+  //         const rect = e.currentTarget.getBoundingClientRect();
+  //         const x = e.clientX - rect.left - rect.width / 2;
+  //         const y = e.clientY - rect.top - rect.height / 2;
+
+  //         xTo.current(x);
+  //         yTo.current(y);
+  //       }
+  //     }),
+  //     [contextSafe]
+  //   );
+
+  //   useGSAP(
+  //     () => {
+  //       if (btnWrapperRef.current) {
+  //         gsap.to(btnWrapperRef.current, {
+  //           rotation: 360,
+  //           duration: 2,
+  //           repeat: -1,
+  //           yoyo: true,
+  //         });
+  //       }
+  //     },
+  //     { scope: container }
+  //   );
+
   return (
     <div className={style.formWrapper}>
       <div className={style.formContainer}>
         <form action="" className={style.form}>
           <div className={style.formData}>
             <label htmlFor="username">Введите имя пользователя</label>
-            <input type="text" id="username" required />
+            <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
           </div>
           <div className={style.formData}>
             <label htmlFor="password">Введите пароль</label>
-            <input type="password" id="password" required />
+            <input type="password" id="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} required />
           </div>
           <div className={style.btnWrapper}>
             <Button callBack={handlerLogin} value={"Войти"} />
